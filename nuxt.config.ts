b9c55cf520c6e248f503afffe0cab93bcea189fc
Arturo Abreu <r2work@pm.me>
2025-07-22 16:24:23 +0100

    chore: add tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
+import material from "@primeuix/themes/material"
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: false },
+  modules: ['@primevue/nuxt-module', '@nuxtjs/tailwindcss'],
+  primevue: {
+    options: {
+      theme: {
+        preset: material
+      }
+    }
+  },
+  tailwindcss: {
+    exposeConfig: true,
+    viewer: true,
+  }
 })
